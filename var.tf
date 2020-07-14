ff17616ac9db1ebf8569abca47184b8bd63749b0
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-15 03:34:25 +0530

    Update var.tf

--- var.tf
+++ var.tf
@@ -1,1 +1,19 @@
 
+variable "client_id" {
+  description = "The Client ID (appId) for the Service Principal used for the AKS deployment"
+}
+
+variable "client_secret" {
+  description = "The Client Secret (password) for the Service Principal used for the AKS deployment"
+}
+
+variable "subscription_id" {
+  description = "The Subscription ID for the Service Principal used for the AKS deployment"
+}
+
+variable "tenant_id" {
+  description = "The Tenant Id for the Service Principal used for the AKS deployment"
+}
+
+
+
