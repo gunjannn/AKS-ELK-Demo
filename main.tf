cd11920cd130a21a6fc6298aa22270d7aea2ff6a
gunjannn <47901460+gunjannn@users.noreply.github.com>
2020-07-15 03:33:41 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,41 @@
+provider "azurerm" {
+  subscription_id = var.subscription_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
+  tenant_id       = var.tenant_id
 
+  version =  "~> 2.18"
+  features {}
+}
+
+resource "azurerm_resource_group" "azkubernetes" {
+  name     = "azkubernetes"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "gitops-demo-aks"
+  location            = azurerm_resource_group.azkubernetes.location
+  resource_group_name = azurerm_resource_group.azkubernetes.name
+  dns_prefix          = "gitlab"
+
+  default_node_pool {
+    name            = "default"
+    node_count      = 1
+    vm_size         = "Standard_D2s_v3"
+    os_disk_size_gb = 30
+  }
+  
+  identity {
+    type = "SystemAssigned"
+    }
+
+  tags = {
+    Terraform = "True"
+  }
+  
+ /* output "env-dynamic-url" {
+     value = azurerm_kubernetes_cluster.aks.kube_config.0.host
+}*/
+  }
+  
